2c836e9082753d25d2d8b38e51ada9a8869c5ade
Khai Gene <khaigenequah100@gmail.com>
2022-04-06 13:39:37 +0800

    Create register layout, button to link to the layout

--- app/src/main/java/me/chunfai/assignment/MainActivity.kt
+++ app/src/main/java/me/chunfai/assignment/MainActivity.kt
@@ -1,11 +1,23 @@
 package me.chunfai.assignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button_click = findViewById<Button>(R.id.button)
+        button_click.setOnClickListener{
+            val intent = Intent(this,Register::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/me/chunfai/assignment/register.kt
@@ -0,0 +1,12 @@
+package me.chunfai.assignment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Register :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register)
+
+    }
+}
