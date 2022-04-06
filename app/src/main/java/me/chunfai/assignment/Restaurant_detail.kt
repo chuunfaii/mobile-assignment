cfd595ed434cf61004626210e7a16ffcd20b0115
Khai Gene <khaigenequah100@gmail.com>
2022-04-07 03:22:31 +0800

    Create Restaurant detail page

---
+++ app/src/main/java/me/chunfai/assignment/Restaurant_detail.kt
@@ -0,0 +1,11 @@
+package me.chunfai.assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Restaurant_detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant_detail)
+    }
+}
