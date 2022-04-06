32bd8034944ee5c620b823f8c1cb8db833cb4293
Chun Fai <ashchunfai@gmail.com>
2022-04-07 00:23:09 +0800

    Add email property to the User class

--- app/src/main/java/me/chunfai/assignment/UserProfile.kt
+++ app/src/main/java/me/chunfai/assignment/UserProfile.kt
@@ -2,10 +2,33 @@ package me.chunfai.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import me.chunfai.assignment.databinding.ActivityUserProfileBinding
 
 class UserProfile : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUserProfileBinding
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_profile)
+        binding = ActivityUserProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+        database = FirebaseFirestore.getInstance()
+
+        val user = intent.getSerializableExtra("user") as? User
+
+        if (user != null) {
+            binding.editFirstName.setText(user.firstName)
+            binding.editLastName.setText(user.firstName)
+        }
     }
+
 }
