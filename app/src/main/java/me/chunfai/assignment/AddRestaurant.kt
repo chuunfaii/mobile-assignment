ede43d00fbf7938ea7785d31c444c3deabdaed50
yeehangchiam <chiamyeehang@gmail.com>
2022-04-07 16:53:29 +0800

    Fix upload image

--- app/src/main/java/me/chunfai/assignment/AddRestaurant.kt
+++ app/src/main/java/me/chunfai/assignment/AddRestaurant.kt
@@ -1,18 +1,25 @@
 package me.chunfai.assignment
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import me.chunfai.assignment.databinding.ActivityAddRestaurantBinding
+import java.io.FileNotFoundException
+import java.io.InputStream
+import java.util.*
 
 class AddRestaurant : AppCompatActivity() {
 
@@ -41,17 +48,45 @@ class AddRestaurant : AppCompatActivity() {
     }
 
     private fun imageChooser(){
-        val intent = Intent()
-        intent.type = "image/*"
-        intent.action = Intent.ACTION_GET_CONTENT
-        startActivityForResult(Intent.createChooser(intent, "Select Image"), pickImage)
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.setType("image/*")
+        someActivityResultLauncher.launch(intent)
+
+//        val intent = Intent()
+//        intent.type = "image/*"
+//        intent.action = Intent.ACTION_GET_CONTENT
+//        startActivityForResult(Intent.createChooser(intent, "Select Image"), pickImage)
+
+//        val intent = Intent()
+//        intent.type = "image/*"
+//        intent.action = Intent.ACTION_GET_CONTENT
+
+//        Log.d("imageChooser()", "Done initializing intent.")
+
+//        Log.d("imageChooser()", "Before launching getResult.")
+
+//        getResult.launch(intent)
+
+//        Log.d("imageChooser()", "After launching getResult.")
+//        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+//        registerForActivityResult(gallery, pickImage)
+
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == RESULT_OK && requestCode == pickImage) {
-            imageUri = data?.data
-            imageView.setImageURI(imageUri)
+    private val someActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            val data = it.data
+            val selectedImage = Objects.requireNonNull(data)?.data
+            var imageStream: InputStream? = null
+
+            try {
+                imageStream = contentResolver.openInputStream(selectedImage!!)!!
+            } catch (error: FileNotFoundException) {
+                Log.d("activityLauncher", error.toString())
+            }
+
+            BitmapFactory.decodeStream(imageStream)
+            binding.imageView.setImageURI(selectedImage)
         }
     }
 
