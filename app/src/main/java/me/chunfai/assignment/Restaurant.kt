1eb4ba5b494f5eca7127f932b9e0147348d2caff
CallMeJaX <87804946+C411M3J4X@users.noreply.github.com>
2022-04-07 16:12:29 +0800

    add restaurant

---
+++ app/src/main/java/me/chunfai/assignment/Restaurant.kt
@@ -0,0 +1,11 @@
+package me.chunfai.assignment
+
+data class Restaurant(
+    var name: String? = null,
+    var address: String? = null,
+    var openTime: String? = null,
+    var closeTime: String? = null,
+    var contact: String? = null,
+    var description: String? = null,
+)
+
