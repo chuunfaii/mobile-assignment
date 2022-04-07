532c0623dd20b8c3ccdcab9f196a2f73a36277de
Khai Gene <khaigenequah100@gmail.com>
2022-04-07 17:23:27 +0800

    Modified restaurant detail

--- app/src/main/java/me/chunfai/assignment/Restaurant_detail.kt
+++ app/src/main/java/me/chunfai/assignment/Restaurant_detail.kt
@@ -2,10 +2,52 @@ package me.chunfai.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.Toast
 
 class Restaurant_detail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurant_detail)
+
+
+//        menu()
+    }
+
+//    private fun menu(){
+//        val menu = findViewById<ImageView>(R.id.option_menu)
+//        val popUpMenu =  PopupMenu(applicationContext,menu)
+//        popUpMenu.inflate(R.menu.menu)
+//        popUpMenu.setOnMenuItemClickListener {
+//            when(it.itemId){
+//                R.id.action_edit->{
+//                    Toast.makeText(applicationContext,"Edit Pressed",Toast.LENGTH_SHORT).show()
+//                    true
+//                }
+//                R.id.action_delete->{
+//                    Toast.makeText(applicationContext,"Delete Pressed",Toast.LENGTH_SHORT).show()
+//                    true
+//                }
+//                else ->true
+//            }
+//        }
+
+//        menu.setOnLongClickListener{
+//            try{
+//                val popup = PopupMenu::class.java.getDeclaredField("mPopup")
+//                popup.isAccessible = true
+//                val menus = popup.get(popUpMenu)
+//                menus.javaClass
+//                    .getDeclaredMethod("setForcedShowIcon",Boolean::class.java)
+//                    .invoke(menus,true)
+//
+//            }catch(e:Exception){
+//                e.printStackTrace()
+//            }finally {
+//                popUpMenu.show()
+//            }
+//            true
+//        }
     }
 }
